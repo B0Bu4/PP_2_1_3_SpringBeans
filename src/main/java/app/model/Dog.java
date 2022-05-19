29eb55b8301601b1541aeea3f5e97f7744da5a7c
B0Bu4 <bobu41@yandex.ru>
2022-05-19 19:20:18 +0300

    commit

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,9 +1,12 @@
 package app.model;
 
 import org.springframework.stereotype.Component;
+import java.io.Serializable;
 
 @Component
-public class Dog extends Animal{
+public class Dog extends Animal implements Serializable {
+    public Dog(){
+    }
     @Override
     public String toString() {
         return "Im a Dog";
