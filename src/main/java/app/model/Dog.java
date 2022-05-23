d172e0c5c10591aaa12a99198233bd92b7e97879
B0Bu4 <bobu41@yandex.ru>
2022-05-23 13:17:21 +0300

    commit

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,15 +1,46 @@
 package app.model;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Component("dog")
 public class Dog extends Animal implements Serializable {
 
+    private String dogSay = "Im a Dog";
+
+    public Dog(){
+
+    }
+
+    public Dog(String dogSay) {
+        this.dogSay = dogSay;
+    }
+
+    public String getDogSay() {
+        return dogSay;
+    }
+
+    public void setDogSay(String dogSay) {
+        this.dogSay = dogSay;
+    }
+
+
     @Override
-    @Bean
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return dogSay.equals(dog.dogSay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dogSay);
+    }
+
+    @Override
     public String toString() {
-        return "Im a Dog";
+        return dogSay;
     }
 }
